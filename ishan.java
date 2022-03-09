af3370811a9bb0e547cc0e0f14b13acf8a831490
ishan shetty <shettyishan4@gmail.com>
2022-03-09 14:16:22 -0500

    ishan
    partnerb

--- ishan.java
+++ ishan.java
@@ -1,6 +1,33 @@
-public class Ishan{
+import java.util.*;
+public abstract class Ishan implements inter{
 
 
+    public boolean getWatched(int a)
+    {
+        int sr = (int)Math.sqrt(a);
 
-    
+        if((sr * sr) == a)
+        	return true;
+        else
+        	return false;
+    }
+
+    public static void main (String[]args)
+		{
+			//System.out.println("hello");
+
+
+		}
+
 }
+
+	interface inter
+		{
+			final int a = (int)(Math.random() * 100);
+
+			public boolean getWatched();
+
+
+		}
+
+
